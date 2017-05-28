3a020d2e6e8fc6b78f6201a1a84cee93f46731bf
Nikita <nks.off@gmail.com>
2017-05-28 21:24:32 +0300

    <init> trying to get info from facebook

--- routes/auth.php
+++ routes/auth.php
@@ -11,20 +11,20 @@ use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/me', function (Request $request) {
     return ['user' => $request->user()];
-});
+})->name('user');
 
 Route::get('/fb/redirect', function (Request $request) {
-    $redirectUrl = route('fb-login');
-    //var_dump($redirectUrl);
-    return Socialite::driver('facebook')->scopes([
-        'email', 'user_likes',
-    ])->with(['redirect_uri' => $redirectUrl,])->redirect();
-});
+    return Socialite::driver('facebook')
+        ->scopes(['email', 'user_likes',])
+        ->with(['redirect_uri' => route('fb-login'),])
+        ->redirect();
+})->name('fb-redirect');
 
 Route::get('/fb/login', function (Request $request) {
-
+    $u = Socialite::driver('facebook')->user();
+    var_dump($u);
+    exit;
 })->name('fb-login');
 
 Route::get('/logout', function (Request $request) {
-
-});
+})->name('logout');
