06b56419ea52240ec1577c7864668aac4d8870bc
Nikita <nks.off@gmail.com>
2017-05-31 22:51:05 +0300

    <init> fix avatar_url fillable field

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'fb_id',
+        'name', 'email', 'password', 'fb_id', 'avatar_url',
     ];
 
     /**
